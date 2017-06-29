6c061ae6f848b76a87c84b31226e4f7107374dde
Iaroslav Nikishyn <lanikis@247e.com>
2017-06-29 17:03:52 +0300

    added base domain models

---
+++ TravelPlanner.DomainModel/Trip.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace TravelPlanner.DomainModel
+{
+    public class Trip
+    {
+        public Guid Id { get; set; }
+        public Guid CreatorId { get; set; }
+        public Guid ChatId { get; set; }
+        public string Title { get; set; }
+        public string Description  { get; set; }
+    }
+
+    public class Invitation
+    {
+        public Guid Id { get; set; }
+        public Guid TripId { get; set; }
+        public Guid? UserId { get; set; }
+        public string Phone { get; set; }
+        public bool IsAccepted => UserId.HasValue;
+    }
+
+    public class Chat
+    {
+        public Guid Id { get; set; }
+        public Guid TripId { get; set; }
+    }
+
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public Guid ChatId { get; set; }
+        public Guid UserId { get; set; }
+        public string Text { get; set; }
+    }
+}
