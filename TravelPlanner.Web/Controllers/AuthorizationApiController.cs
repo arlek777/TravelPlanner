158fcedb644b58e530b669a8172bebbe3e11f9d0
Iaroslav Nikishyn <lanikis@247e.com>
2017-06-30 16:55:06 +0300

    added empty authorization controller

---
+++ TravelPlanner.Web/Controllers/AuthorizationApiController.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TravelPlanner.Web.Controllers
+{
+    public class AuthorizationApiController : Controller
+    {
+    }
+}
