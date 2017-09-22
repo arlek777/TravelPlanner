5cb04c7afcd1084e4b48a2439a0a4440a5c20202
Iaroslav <arlek.n93@gmail.com>
2017-09-22 23:54:34 +0300

    working on dictinonary way to write translations

--- TravelPlanner.TranslationKeysExtracter/Program.fs
+++ TravelPlanner.TranslationKeysExtracter/Program.fs
@@ -5,17 +5,34 @@ open System
 open System.IO
 open System.Text
 open System.Text.RegularExpressions
+open System.Web.Script.Serialization
 
 [<EntryPoint>]
 let main argv = 
-    let appFolderPath = @"C:\Users\nii\Documents\Visual Studio 2017\Projects\TravelPlanner\TravelPlanner.Web\ClientApp\app\"
-    let getHtmlFilePathes = Directory.GetFiles (appFolderPath, "*.html", SearchOption.AllDirectories)
+    let appFolderPath = @"C:\Users\Arlek\Source\Repos\TravelPlanner\TravelPlanner.Web\ClientApp\app\"
+    let translationFilePath = @"C:\Users\Arlek\Source\Repos\TravelPlanner\TravelPlanner.Web\wwwroot\ru.json"
+
+    let createTranslationObject path = 
+        File.ReadAllText(path)
+        |> (new JavaScriptSerializer()).DeserializeObject
+
+    let writeTranslationKey (key:string) = 
+        createTranslationObject translationFilePath
+        |> Seq.ca
 
     let regexMatches pattern (input: string) =
         Regex.Matches(input, pattern)
+        |> Seq.cast<Match>
+        |> Seq.map(fun m -> m.Value)
+        |> Seq.toArray
 
-    let getTranslationKeys (path: string) = 
-        File.ReadAllText(path)
-        |> regexMatches @"(?<=\blocalize="")[^""]*"
-        |> List.map (fun m -> m
-    0 // return an integer exit code
+    let writeTranslationKeys files = 
+        files
+        |> Array.iter(fun f -> 
+            File.ReadAllText(f)
+            |> regexMatches @"(?<=\blocalize="")[^""]*"
+            |> Array.iter (fun k -> writeTranslationKey k))
+
+    writeTranslationKeys (Directory.GetFiles(appFolderPath, "*.html", SearchOption.AllDirectories))
+
+    0
