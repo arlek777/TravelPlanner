9a17847fe9c86887328d23013e450d342d01b28f
Iaroslav Nikishyn <iaroslav.nikishyn@7digital.com>
2017-09-22 18:09:36 +0300

    working on f# project

---
+++ TravelPlanner.TranslationKeysExtracter/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open System
+open System.IO
+open System.Text
+open System.Text.RegularExpressions
+
+[<EntryPoint>]
+let main argv = 
+    let appFolderPath = @"C:\Users\nii\Documents\Visual Studio 2017\Projects\TravelPlanner\TravelPlanner.Web\ClientApp\app\"
+    let getHtmlFilePathes = Directory.GetFiles (appFolderPath, "*.html", SearchOption.AllDirectories)
+
+    let regexMatches pattern (input: string) =
+        Regex.Matches(input, pattern)
+
+    let getTranslationKeys (path: string) = 
+        File.ReadAllText(path)
+        |> regexMatches @"(?<=\blocalize="")[^""]*"
+        |> List.map (fun m -> m
+    0 // return an integer exit code
