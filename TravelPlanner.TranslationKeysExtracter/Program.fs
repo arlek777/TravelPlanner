6a8536826a0717b521eaa39147492a215b516847
Iaroslav Nikishyn <iaroslav.nikishyn@7digital.com>
2017-09-25 18:14:48 +0300

    version of fsharp parse done

--- TravelPlanner.TranslationKeysExtracter/Program.fs
+++ TravelPlanner.TranslationKeysExtracter/Program.fs
@@ -5,17 +5,43 @@ open System
 open System.IO
 open System.Text
 open System.Text.RegularExpressions
+open System.Web.Script.Serialization
+open System.Collections.Generic
 
 [<EntryPoint>]
 let main argv = 
     let appFolderPath = @"C:\Users\nii\Documents\Visual Studio 2017\Projects\TravelPlanner\TravelPlanner.Web\ClientApp\app\"
-    let getHtmlFilePathes = Directory.GetFiles (appFolderPath, "*.html", SearchOption.AllDirectories)
+    let translationFile = @"C:\Users\nii\Documents\Visual Studio 2017\Projects\TravelPlanner\TravelPlanner.Web\wwwroot\ru.json";
+    let localizeRegexPattern = @"(?<=\blocalize="")[^""]*"
 
-    let regexMatches pattern (input: string) =
+    let getTranslationsFromFile = 
+        translationFile
+        |> File.ReadAllText
+        |> JavaScriptSerializer().Deserialize<Dictionary<string, string>>
+
+    let addTranslationKeys (translations: Dictionary<string, string>) keys = 
+        keys
+        |> Array.iter(fun k -> if not (translations.ContainsKey k) then translations.Add(k, ""))
+        translations
+
+    let writeTranslationsToFile translations  = 
+        File.WriteAllText(translationFile, JavaScriptSerializer().Serialize translations)
+
+    let getHtmlFiles = Directory.GetFiles (appFolderPath, "*.html", SearchOption.AllDirectories)
+
+    let regexMatches pattern (input: string)  =
         Regex.Matches(input, pattern)
-
-    let getTranslationKeys (path: string) = 
-        File.ReadAllText(path)
-        |> regexMatches @"(?<=\blocalize="")[^""]*"
-        |> List.map (fun m -> m
+        |> Seq.cast<Match>
+        |> Seq.map(fun (m: Match) -> m.Value)
+        |> Seq.toArray
+    
+    let execute = 
+        getHtmlFiles
+        |> Array.iter(fun f -> 
+            f
+            |> File.ReadAllText
+            |> regexMatches localizeRegexPattern
+            |> addTranslationKeys getTranslationsFromFile
+            |> writeTranslationsToFile)
+    
     0 // return an integer exit code
